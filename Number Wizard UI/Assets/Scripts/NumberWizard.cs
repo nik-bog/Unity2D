17fa95c9cc4140fa7362839446e8678c8d39e057
Nikita Bogdanov <nikbogdan2002@gmail.com>
2023-04-05 23:19:37 +0300

    Add Number Wizard

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+    [SerializeField] Button higherButton;
+    [SerializeField] Button lowerButton;
+
+    int guess;
+
+    private void Start()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+        CheckRange();
+    }
+
+    void CheckRange()
+    {
+        if (guessText.text == "1")
+        {
+            lowerButton.interactable = false;
+        }
+
+        else if (guessText.text == "1000")
+        {
+            higherButton.interactable = false;
+        }
+
+        else
+        {
+            higherButton.interactable = true;
+            lowerButton.interactable = true;
+        }
+    }
+}
