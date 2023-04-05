499f52525dd8409e9a603384d0fc8aad0ee9cde2
Nikita Bogdanov <nikbogdan2002@gmail.com>
2023-04-06 00:49:13 +0300

    Add Laser Defender project

---
+++ Laser Defender/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+    Material myMaterial;
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
---
+++ Laser Defender/Assets/Scripts/GameSession.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        int numberGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numberGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
